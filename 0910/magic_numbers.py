9fd6f474620e02253b64ec564371822124701b9c
Fngje <fngje0128@163.com>
2017-09-10 20:18:15 +0800

    2017年9月10日20:17:24，if条件语句的各种使用

---
+++ 0910/magic_numbers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#Author:jie.fang
+
+# answer = 17
+# if answer != 42:
+#     print("That is not the corrent answer.Please try again.")
+
+#还可以通过and来检查多个条件
+# age0 = 22
+# age1 = 18
+# print(age0 >= 21 and age1 >=21)
+# age1 = 22
+# print(age0 >= 21 and age1 >=21)
+
+#检查特定的值是否包含在列表中
+requested_toppings = ['mushrooms','onions','pineapple']
+print('mushrooms' in requested_toppings)
+print('pepperoni' in requested_toppings)
