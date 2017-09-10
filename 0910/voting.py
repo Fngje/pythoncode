981408d900723a824bc0f8cdb3641dea747b634a
Fngje <fngje0128@163.com>
2017-09-10 20:24:22 +0800

    2017年9月10日20:23:28，if...else...

---
+++ 0910/voting.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#Author:jie.fang
+
+age = 19
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry,you are too young to vote.")
+    print("Please register to vote as soon as you turn 18.")
