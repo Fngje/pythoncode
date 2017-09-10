075c04997bfd8fb004cb16b58c4202db0c3a7c70
Fngje <fngje0128@163.com>
2017-09-10 20:18:34 +0800

    2017年9月10日20:17:24，if条件语句的各种使用

---
+++ 0910/toppings.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#Author:jie.fang
+
+'''
+#检查是否不相等
+requested_topping = 'mushrooms'
+if requested_topping != 'auchovies':
+    print("Hold the auchovies!")
+'''
+
+#比较数字
+age = 18
+print(age == 18)
