68cfd605683e0d4dfc54ae5746a5eba6528577b8
Fngje <fngje0128@163.com>
2017-10-03 23:55:17 +0800

    2017年10月3日23:54:46，项目《外星人入侵》001

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -4,17 +4,25 @@
 
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    ship = Ship(screen)
+
+    #bg_color = (230,230,230)
     while True:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         pygame.display.flip()
 run_game()
